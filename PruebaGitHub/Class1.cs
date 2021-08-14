b73fbf5be03d7aa7b9bb278d12012c483f3bc1f5
Luis Hernández Gómez <ing.luishg76@gmail.com>
2021-08-14 16:00:04 -0400

    agregue clase

---
+++ PruebaGitHub/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGitHub
+{
+    class Class1
+    {
+        //Al fin aqui estoy yo
+    }
+}
